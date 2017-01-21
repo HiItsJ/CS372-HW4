ca730361d8cd5b7e68f327220a00d22dd1d403d6
Josiah Gallegos <jgallegos20@my.whitworth.edu>
2017-01-20 16:20:26 -0800

    Web spider progress: able to add a single link found from the original link provided.

---
+++ WebSpider.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+import java.util.regex.*;
+
+public class WebSpider{
+
+  static HashMap<URL, Boolean> links;
+
+  public static void main(String[] args){
+    links = new HashMap<URL, Boolean>();
+    try{
+      links.put(new URL("http://www.whitworth.edu/cms/"), false);
+    }
+    catch(Exception ex){
+      System.out.printf("Oops: %s", ex.getMessage());
+    }
+    search();
+  }
+  public static void search(){
+    for (URL url: links.keySet()){
+      if (!links.get(url)){
+        links.put(url, true);
+        try{
+          BufferedReader rdr = new BufferedReader(new InputStreamReader(url.openStream()));
+          String line;
+          while ((line = rdr.readLine()) != null){
+            Pattern p = Pattern.compile("href=\"(.*?)\"");
+            Matcher matcher = p.matcher(line);
+            if (matcher.find()){
+              String s = matcher.group(1);
+              if (s.contains("http") || s.contains("www")){
+                if (s.contains(".com") || s.contains(".net") || s.contains(".edu") || s.contains(".org")){
+                  URL u = new URL(s);
+                  if (links.get(u) == null && links.size()<100){
+                    if (links.size()<100){
+                      links.put(u, false);
+                      System.out.println("added: "+s);
+                    }
+                    else{
+                      System.out.println("break");
+                      break;
+                    }
+                  }
+                }
+              }
+              else{
+                System.out.println("found in map");
+              }
+            }
+            else{
+              System.out.println("no match");
+            }
+          }
+        }
+        catch(Exception ex){
+          System.out.printf("Error: %s\n", ex.getMessage());
+        }
+      }
+    }
+    System.out.println(links.size());
+  }
+}
